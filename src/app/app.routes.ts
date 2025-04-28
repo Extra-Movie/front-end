35c5f2954a526bf270c500b2586812f459785544
kareem172 <kareemkhalaf1722@gmail.com>
2025-04-28 07:02:07 +0300

    add basic structure for the routes and pages and layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { NavLayoutComponent } from './layouts/nav-layout/nav-layout.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { ErrorComponent } from './pages/error/error.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: NavLayoutComponent,
+    children: [
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      {
+        path: 'home',
+        component: HomeComponent,
+      },
+    ],
+  },
+  {
+    path: 'error',
+    component: ErrorComponent,
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+  },
+];
---
+++ src/app/layouts/nav-layout/nav-layout.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-nav-layout',
+  imports: [RouterOutlet],
+  templateUrl: './nav-layout.component.html',
+})
+export class NavLayoutComponent {}
