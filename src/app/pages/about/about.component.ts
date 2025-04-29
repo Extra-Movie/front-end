9232087631a7516859ccdbbfe34118cc86b4d2f0
ghadaahmed <ghadaahmed2772002@gmail.com>
2025-04-29 20:39:34 +0300

    this about and contact page by ghada

---
+++ src/app/pages/about/about.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+
+@Component({
+  selector: 'app-about',
+  imports: [NgIcon],
+  templateUrl: './about.component.html',
+  styles: ``
+})
+export class AboutComponent {
+
+}
