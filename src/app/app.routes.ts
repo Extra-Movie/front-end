00fc1028e5ddebd20bc889e75b9da29036656a7a
ghadaahmed <ghadaahmed2772002@gmail.com>
2025-04-30 00:48:49 +0300

    added after color update text white

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,15 @@ import { NavLayoutComponent } from './layouts/nav-layout/nav-layout.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { ErrorComponent } from './pages/error/error.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: NavLayoutComponent,
     children: [
-      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: '', redirectTo: 'home', pathMatch: 'full' },  //change again ghada
       {
         path: 'home',
         component: HomeComponent,
@@ -20,6 +22,12 @@ export const routes: Routes = [
     path: 'error',
     component: ErrorComponent,
   },
+  {
+    path:'about',component:AboutComponent
+  },  //about
+  {
+    path:'contactUs',component:ContactUsComponent
+  },  //contact us
   {
     path: '**',
     component: NotFoundComponent,
