2a977f0cc90d0d40a55018a42dff3d8d45f40342
kareem172 <kareemkhalaf1722@gmail.com>
2025-05-01 22:17:35 +0300

    add toaster container component to app component for user notifications

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DarkModeService } from './services/dark-mode.service';
 import { AsyncPipe } from '@angular/common';
+import { ToasterContainerComponent } from "./components/toaster/toaster-container/toaster-container.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AsyncPipe],
+  imports: [RouterOutlet, AsyncPipe, ToasterContainerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
