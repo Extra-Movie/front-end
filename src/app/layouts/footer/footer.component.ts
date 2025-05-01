07c7f03e9bd83a200f3178b5c0382a1e5efae53a
Maha Elshazly <mahaelshazly32@gmail.com>
2025-05-02 00:39:14 +0300

    footer update

--- src/app/layouts/footer/footer.component.ts
+++ src/app/layouts/footer/footer.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
 
+import { Component } from '@angular/core';
+import {NgIcon ,provideIcons} from '@ng-icons/core';
+import {faBrandSquareFacebook,faBrandSquareXTwitter,faBrandSquareInstagram,faBrandSquareYoutube} from "@ng-icons/font-awesome/brands"
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [NgIcon],
   templateUrl: './footer.component.html',
   styles: ``,
-
+  providers: [provideIcons({faBrandSquareFacebook,faBrandSquareXTwitter,faBrandSquareInstagram,faBrandSquareYoutube})]
 })
 export class FooterComponent {
 
--- src/app/layouts/nav-layout/nav-layout.component.ts
+++ src/app/layouts/nav-layout/nav-layout.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-nav-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent],
   templateUrl: './nav-layout.component.html',
 })
 export class NavLayoutComponent {}
