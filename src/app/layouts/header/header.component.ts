713f7cf4d0f903d78b30e6e992157047f1faf88d
kareem172 <kareemkhalaf1722@gmail.com>
2025-05-02 06:02:04 +0300

    refactor: enhance search input with icon and update view providers in header

--- src/app/layouts/header/header.component.ts
+++ src/app/layouts/header/header.component.ts
@@ -3,12 +3,13 @@ import { Component } from '@angular/core';
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import { faSolidCartShopping } from '@ng-icons/font-awesome/solid';
 import { solarMoonStarsBold ,solarSun2Bold} from '@ng-icons/solar-icons/bold';
+import { ionSearch } from '@ng-icons/ionicons';
 @Component({
   selector: 'app-header',
   imports: [CommonModule,NgIcon],
   templateUrl: './header.component.html',
   styles: ``,
-  viewProviders: [provideIcons({ faSolidCartShopping ,solarMoonStarsBold,solarSun2Bold})]
+  viewProviders: [provideIcons({ faSolidCartShopping ,solarMoonStarsBold,solarSun2Bold, ionSearch})]
 })
 export class HeaderComponent {
   menuOpen = false;
