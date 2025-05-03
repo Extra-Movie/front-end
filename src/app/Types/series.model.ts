0a18df7ff52e80e28158ca5a78171331cece62ab
ghadaahmed <ghadaahmed2772002@gmail.com>
2025-05-03 16:08:54 +0300

    add series type in series.model.ts file

---
+++ src/app/Types/series.model.ts
@@ -0,0 +1,18 @@
+
+export interface Series{
+name	:string
+original_name:	string
+backdrop_path:	string
+poster_path	:string
+genre_ids	:object
+first_air_date:	string
+popularity:	number
+vote_average:	number
+vote_count:	number
+origin_country:object
+original_language:	string
+adult:	boolean
+overview:	string
+price:	number
+number_of_purchases:	number
+}
