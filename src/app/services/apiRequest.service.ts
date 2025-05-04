81bb16aa92c5761a68215365295eb1a942714d29
kareem172 <kareemkhalaf1722@gmail.com>
2025-05-04 12:08:29 +0300

    feat: implement general genetic RequestStateService for managing API request states

---
+++ src/app/services/apiRequest.service.ts
@@ -0,0 +1,53 @@
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { catchError, finalize, map, tap } from 'rxjs/operators';
+import { Injectable, signal } from '@angular/core';
+
+export interface RequestState<T> {
+  loading: boolean;
+  success: boolean;
+  error: string | null;
+  data: T | null;
+}
+
+@Injectable({ providedIn: 'root' })
+export class RequestStateService<T> {
+  private initialState: RequestState<T> = {
+    loading: false,
+    success: false,
+    error: null,
+    data: null,
+  };
+
+  response = signal<RequestState<T>>(this.initialState);
+
+  track(observable: Observable<T>): Observable<T | null> {
+    this.response.set({
+      loading: true,
+      success: false,
+      error: null,
+      data: null,
+    });
+    return observable.pipe(
+      tap(
+        (data) =>
+          this.response.update((prev) => ({ ...prev, data, success: true })) // Update the data and success state
+      ),
+      catchError((error) => {
+        console.error('Error occurred:', error); // Log the error to the console
+        this.response.update((prev) => ({
+          ...prev,
+          error: error.error.message,
+          success: false,
+        })); // Update the error state
+        return of(null);
+      }),
+      finalize(
+        () => this.response.update((prev) => ({ ...prev, loading: false })) // Set loading to false after the observable completes
+      )
+    );
+  }
+
+  reset() {
+    this.response.set(this.initialState);
+  }
+}
