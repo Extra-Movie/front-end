c2f93aca2abe5077c962fc620df3854f943177bb
Eslam-Elwey <eslam.elwey@gmail.com>
2025-05-06 02:01:14 +0300

    add Movie Response , movie , genre , filter status , filtervalues types

---
+++ src/app/Types/Movie.types.ts
@@ -0,0 +1,53 @@
+export type MovieType =
+{
+  _id : string ,
+  id: number ,
+  original_language : string ,
+  title	: string ,
+  original_title	: string ,
+  backdrop_path	: string ,
+  poster_path	: string ,
+  genre_ids	: number[] ,
+  release_date	: string ,
+  popularity	: number ,
+  vote_average	: number ,
+  vote_count	: number ,
+  adult	: boolean ,
+  video	: boolean ,
+  overview	: string ,
+  price	: number ,
+  number_of_purchases : number
+}
+
+export type MovieResponseType = {
+  movies : MovieType[] ,
+  page : number ,
+  totalPages : number ,
+  totalMovies : number
+}
+
+
+export type MovieGenreType = {
+  id :	number ,
+  name :	string
+}
+
+export type MovieFilterStatusType = {
+
+  //true => will Apply filter with it
+  nameState : boolean ,
+  yearState : boolean ,
+  genreState : boolean ,
+  voteState : boolean ,
+  popularityState : boolean
+}
+
+export type MovieFilteredValuesType = {
+
+  //Values To Apply Filteration
+  nameValue : string ,
+  yearValue : string ,
+  genreValue : number ,
+  voteValue : number ,
+  popularityValue : number
+}
