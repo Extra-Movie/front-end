b200641183b03c4f1e67b13af0ad16068cb76503
Maha Elshazly <mahaelshazly32@gmail.com>
2025-05-06 16:14:02 +0300

    update to send values only on submit

--- src/app/pages/filter/filter/filter.component.ts
+++ src/app/pages/filter/filter/filter.component.ts
@@ -2,7 +2,7 @@ import { Component,OnInit } from '@angular/core';
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import { faSolidFilter } from '@ng-icons/font-awesome/solid';
 import { FormBuilder, FormGroup, FormsModule, Validators } from '@angular/forms';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 @Component({
@@ -18,9 +18,104 @@ export class FilterComponent implements OnInit{
 
   constructor(
     private fb: FormBuilder,
-    private router: Router,
-    private route: ActivatedRoute
+    private router: Router
   ) {}
+
+
+  //!--------send all of the form data
+  // onSubmit(): void {
+  //   if (this.filterForm.valid) {
+  //     const raw = this.filterForm.value;
+  
+  //     const selectedGenres = raw.genres
+  //       ? Object.keys(raw.genres).filter((key) => raw.genres[key])
+  //       : [];
+  
+  //     const queryParams: any = {
+  //       search: raw.search,
+  //       rating: raw.rating,
+  //       popularity: raw.popularity,
+  //       genres: selectedGenres.join(','),
+  //       year: raw.year
+  //     };
+  
+  //     this.router.navigate([], {
+  //       queryParams,
+  //       queryParamsHandling: 'merge',
+  //     });
+  //   } else {
+  //     this.filterForm.markAllAsTouched();
+  //   }
+  // }
+  
+
+
+  //^--------send only changed form data
+  onSubmit(): void {
+    if (this.filterForm.valid) {
+      const raw = this.filterForm.value;
+  
+      const defaultValues = {
+        search: '',
+        rating: 3,
+        popularity: 'Most Popular',
+        genre: {
+          action: false,
+          adventure: false,
+          drama: false,
+          comedy: false,
+        },
+        year: '',
+      };
+  
+      const queryParams: any = {};
+  
+      if (raw.search !== defaultValues.search) queryParams.search = raw.search;
+      if (raw.rating !== defaultValues.rating) queryParams.rating = raw.rating;
+      if (raw.popularity !== defaultValues.popularity)
+        queryParams.popularity = raw.popularity;
+      if (raw.year !== defaultValues.year) queryParams.year = raw.year;
+  
+      if (raw.genre) {
+        const selectedGenres = Object.keys(raw.genre).filter(
+          (key) => raw.genre[key]
+        );
+        if (selectedGenres.length > 0) {
+          queryParams.genres = selectedGenres.join(',');
+        }
+      }
+  
+      this.router.navigate([], {
+        queryParams,
+        queryParamsHandling: 'merge',
+      });
+    } else {
+      this.filterForm.markAllAsTouched();
+    }
+  }
+  
+  
+  clearFilters(): void {
+    this.filterForm.reset({
+      search: '',
+      rating: 3,
+      popularity: 'Most Popular',
+      genre: {
+        action: false,
+        adventure: false,
+        drama: false,
+        comedy: false,
+      },
+      year: '',
+    });
+  
+    this.router.navigate([], {
+      queryParams: {},
+    });
+  }
+  
+  
+  
 
   ngOnInit(): void {
     this.filterForm = this.fb.group({
@@ -36,46 +131,7 @@ export class FilterComponent implements OnInit{
       year: ['', [Validators.pattern(/^\d{4}$/)]]
     });
 
-    this.filterForm.valueChanges.subscribe(value => {
-      this.applyFilters();
-    });
+  
   }
 
-  applyFilters(): void {
-    const formValue = this.filterForm.value;
-
-    const selectedGenres = Object.entries(formValue.genre)
-      .filter(([_, selected]) => selected)
-      .map(([genre]) => genre);
-
-    const queryParams: any = {
-      ...(formValue.search && { search: formValue.search }),
-      ...(formValue.rating && { rating: formValue.rating }),
-      ...(formValue.popularity && { popularity: formValue.popularity }),
-      ...(selectedGenres.length > 0 && { genres: selectedGenres.join(',') }),
-      ...(formValue.year && this.filterForm.controls['year'].valid && { year: formValue.year })
-    };
-
-    this.router.navigate([], {
-      relativeTo: this.route,
-      queryParams,
-      queryParamsHandling: 'merge'
-    });
-  }
-
-  clearFilters(): void {
-    this.filterForm.reset({
-      search: '',
-      rating: 3,
-      popularity: 'Most Popular',
-      genre: {
-        action: false,
-        adventure: false,
-        drama: false,
-        comedy: false
-      },
-      year: ''
-    });
-    this.router.navigate([], { relativeTo: this.route, queryParams: {} });
-  }
 }
