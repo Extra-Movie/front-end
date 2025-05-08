36e2b213c0666376cbd89fc4ac379fec36583891
kareem172 <kareemkhalaf1722@gmail.com>
2025-05-08 11:07:00 +0300

    feat: add stripe API key to environment configurations for development and production

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
   apiUrl: 'https://back-end-production-e1e1.up.railway.app/api',
+  stripe_api_key: "pk_test_51RM9bo4Fl551OK2AVH2s6lkvlE2zW0aXUnCrpvP5Zn1wX2JBgknp8vVT4MCgBiTymrh971YkdbxV7hzZKHiz1WMS00V27tySCX"
 };
