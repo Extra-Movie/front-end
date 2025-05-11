d9567ec9892dbee896ad79fafaa9680aab005c17
kareem172 <kareemkhalaf1722@gmail.com>
2025-05-11 10:42:03 +0300

    feat: enhance MediaCard component with watchlist functionality and dynamic icons

--- src/app/components/mediacard/mediacard.component.ts
+++ src/app/components/mediacard/mediacard.component.ts
@@ -3,19 +3,29 @@ import { MovieType } from '../../Types/Movie.types';
 import { Series } from '../../Types/series.model';
 import { RouterLink } from '@angular/router';
 import { CartService } from '../../services/cart.service';
+import { WatchListService } from '../../services/watch-list.service';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { faSolidBookmark, faSolidCartShopping } from '@ng-icons/font-awesome/solid';
+import { faBookmark } from '@ng-icons/font-awesome/regular';
 
 RouterLink;
 
 @Component({
   selector: 'app-mediaCard',
-  imports: [RouterLink],
+  imports: [RouterLink, NgIcon],
   templateUrl: './mediacard.component.html',
-  styles: ``,
+  providers: [provideIcons({faSolidBookmark, faBookmark,faSolidCartShopping})],
 })
 export class MediaCardComponent {
   @Input({ required: true }) mediaItem!: MovieType | Series;
 
   cart = inject(CartService);
+  watchList = inject(WatchListService);
+  isInWatchList = computed(() => {
+    return this.watchList
+      .watchList()
+      ?.some((item) => item.item._id === this.mediaItem._id);
+  });
   isInCart = computed(() => {
     if ('title' in this.mediaItem) {
       return this.cart
@@ -82,5 +92,19 @@ export class MediaCardComponent {
       });
     }
   }
-  addToWatchList() {}
+  addToWatchList() {
+    if ('title' in this.mediaItem) {
+      this.watchList.addToWatchList({
+        kind: 'movies',
+        item: this.mediaItem,
+        _id: this.mediaItem._id,
+      });
+    } else if ('original_name' in this.mediaItem) {
+      this.watchList.addToWatchList({
+        kind: 'tvShows',
+        item: this.mediaItem,
+        _id: this.mediaItem._id,
+      });
+    }
+  }
 }
