a208d0961df2c056e93b9314b59200f1c5eb231f
Eslam-Elwey <eslam.elwey@gmail.com>
2025-05-12 03:45:57 +0300

    remove of unecessary logic of genres and storing it

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,11 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { DarkModeService } from '../../services/dark-mode.service';
 import { ToastService } from '../../services/toast.service';
 
-import { FilterModalComponent } from '../filter/filter-modal/filter-modal.component';
-
-import { MovieService } from '../../services/server/movie.service';
-
-import { MovieGenreType, MovieGenreMatchType } from '../../Types/Movie.types';
 
 import { CommonModule } from '@angular/common';
 import { HeroComponent } from '../../components/hero/hero.component';
@@ -16,47 +11,17 @@ import { CarouselSeriesComponent } from "../../components/carouselseries/carouse
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  imports: [FilterModalComponent, CommonModule, HeroComponent, CarouselComponent, CarouselSeriesComponent],
+  imports: [CommonModule, HeroComponent, CarouselComponent, CarouselSeriesComponent],
 })
 export class HomeComponent implements OnInit {
   constructor(
     private toast: ToastService,
     private theme: DarkModeService,
-    private movieService: MovieService
   ) {}
 
-  movieGeners!: MovieGenreType[];
-  movieGenersIDGenerated: MovieGenreMatchType = {};
-  genreStoreFlag: boolean = false;
+
 
   ngOnInit(): void {
-    //store genres in local storage for one time
-    this.getGenres();
-  }
-
-  //get media genres and store it in local storage to execute only request
-  getGenres() {
-    //geners =>
-    this.movieService.getMovieGeners().subscribe({
-      next: (data: any) => {
-        this.movieGeners = data;
-        for (let i = 0; i < this.movieGeners.length; i++) {
-          this.movieGenersIDGenerated[
-            this.movieGeners[i].name.toLocaleLowerCase()
-          ] = this.movieGeners[i].id;
-        }
-        localStorage.setItem(
-          'genreTypesObj',
-          JSON.stringify(this.movieGenersIDGenerated)
-        );
-        console.log(this.movieGenersIDGenerated);
-      },
-      error: (error) => {
-        console.log(error);
-        this.showErrorToast();
-      },
-      complete: () => console.log('completed Geners'),
-    });
   }
 
   showToast() {
