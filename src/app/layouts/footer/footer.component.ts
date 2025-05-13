86af9001b1cdcac9fbbba46b193d6db501437d08
Maha Elshazly <mahaelshazly32@gmail.com>
2025-05-14 00:03:28 +0300

    update content of footer and linking the pages

--- src/app/layouts/footer/footer.component.ts
+++ src/app/layouts/footer/footer.component.ts
@@ -1,10 +1,10 @@
-
+import { RouterLink , RouterLinkActive } from '@angular/router';
 import { Component } from '@angular/core';
 import {NgIcon ,provideIcons} from '@ng-icons/core';
 import {faBrandSquareFacebook,faBrandSquareXTwitter,faBrandSquareInstagram,faBrandSquareYoutube} from "@ng-icons/font-awesome/brands"
 @Component({
   selector: 'app-footer',
-  imports: [NgIcon],
+  imports: [NgIcon,RouterLink,RouterLinkActive],
   templateUrl: './footer.component.html',
   styles: ``,
   providers: [provideIcons({faBrandSquareFacebook,faBrandSquareXTwitter,faBrandSquareInstagram,faBrandSquareYoutube})]
