d498ef6a7205cdcb0ac85e11c7efef033e1ad1bc
kareem172 <kareemkhalaf1722@gmail.com>
2025-05-21 11:19:55 +0300

    refactor(app.component): enhance user data fetching with authorization check

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, effect } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { DarkModeService } from './services/dark-mode.service';
 import { AsyncPipe } from '@angular/common';
 import { ToasterContainerComponent } from './components/toaster/toaster-container/toaster-container.component';
-import {ScrollProgressComponent}  from './components/scroll-progress/scroll-progress.component'
+import { ScrollProgressComponent } from './components/scroll-progress/scroll-progress.component';
 import { UserService } from './services/user.service';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AsyncPipe, ToasterContainerComponent,ScrollProgressComponent],
+  imports: [
+    RouterOutlet,
+    AsyncPipe,
+    ToasterContainerComponent,
+    ScrollProgressComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
@@ -16,12 +22,17 @@ export class AppComponent {
   constructor(
     public darkModeService: DarkModeService,
     private router: Router,
+    private authService: AuthService,
     private user: UserService
   ) {
     // scroll to top on route change
     this.router.events.subscribe(() => {
       window.scrollTo({ behavior: 'smooth', top: 0, left: 0 });
     });
-    user.getMyData().subscribe();
+    effect(() => {
+      if (authService.isLoggedIn()) {
+        user.getMyData().subscribe();
+      }
+    });
   }
 }
