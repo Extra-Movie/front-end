667b081a10d4248502d11fa160164531dca420af
kareem172 <kareemkhalaf1722@gmail.com>
2025-05-21 13:07:23 +0300

    refactor(camera-view): improve HTML structure and replace button with router link

--- src/app/components/camera-view/camera-view.component.ts
+++ src/app/components/camera-view/camera-view.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import { faSolidCamera ,faSolidBatteryHalf} from '@ng-icons/font-awesome/solid';
 import { bootstrapRecordCircle } from '@ng-icons/bootstrap-icons';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-camera-view',
-  imports: [NgIcon],
+  imports: [NgIcon, RouterLink],
   templateUrl: './camera-view.component.html',
   styles: ``,
   providers: [provideIcons({ faSolidCamera ,faSolidBatteryHalf,bootstrapRecordCircle })],
